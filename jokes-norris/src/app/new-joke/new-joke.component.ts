e458d59ea071d255bbce0b6add38209fc1a8be3a
car <carmelogustavo@gmail.com>
2019-05-14 14:01:53 +0200

    style: layout basic new joke component

--- jokes-norris/src/app/new-joke/new-joke.component.ts
+++ jokes-norris/src/app/new-joke/new-joke.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Joke } from '../Joke';
 @Component({
   selector: 'app-new-joke',
   templateUrl: './new-joke.component.html',
-  styleUrls: ['./new-joke.component.css']
+  styleUrls: ['./new-joke.component.css'],
 })
 export class NewJokeComponent implements OnInit {
+  joke: Joke = {
+    id: 23,
+    joke: 'Chuck Norris is the reason why Waldo is hiding',
+    categories: [],
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
