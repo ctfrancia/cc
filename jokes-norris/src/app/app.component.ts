1cca0efd001f352c56afec0497c831cc50cfb838
car <carmelogustavo@gmail.com>
2019-05-15 08:48:21 +0200

    feat: add favorite angular material dialog

--- jokes-norris/src/app/app.component.ts
+++ jokes-norris/src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+import { MatDialog } from '@angular/material/dialog';
+import { FavoritesJokesComponent } from './favorites-jokes/favorites-jokes.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'jokes-norris';
+
+  constructor(public dialog: MatDialog) {}
+
+  openFavorites(): void {
+    const favoritesRef = this.dialog.open(FavoritesJokesComponent, {
+      width: '250px',
+    });
+
+    favoritesRef.afterClosed().subscribe(result => {
+      console.log('on our way');
+    });
+  }
 }
--- jokes-norris/src/app/app.module.ts
+++ jokes-norris/src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
 import { AppComponent } from './app.component';
 import { NewJokeComponent } from './new-joke/new-joke.component';
+import { FavoritesJokesComponent } from './favorites-jokes/favorites-jokes.component';
 
 @NgModule({
-  declarations: [AppComponent, NewJokeComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent, NewJokeComponent, FavoritesJokesComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+  ],
+  entryComponents: [FavoritesJokesComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
