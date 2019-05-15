abc92eff848761a197f2ae01948f2fe1d9e0ec7e
car <carmelogustavo@gmail.com>
2019-05-15 12:15:29 +0200

    refactor: update joke link and update model and uses\\"

--- jokes-norris-server/server.js
+++ jokes-norris-server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const http = require('http');
 const request = require('request');
 
 const port = process.env.PORT || 5000;
