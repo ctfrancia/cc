8f1d58864441ca4c3eb752f6f943e12ad15cdc51
car <carmelogustavo@gmail.com>
2019-05-15 11:52:12 +0200

    Revert "feat: server request to jokes WIP"
    This reverts commit 337bbcda6a3e74c866859dd6c3ee1fa3b62815c4.

--- jokes-norris-server/server.js
+++ jokes-norris-server/server.js
@@ -1,40 +1,9 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const http = require('http');
-const request = require('request');
 
 const port = process.env.PORT || 5000;
-const filePath = '../jokes-norris';
 
-app.use('/', express.static(filePath));
-
-app
-  .use(
-    cors({
-      origin: 'http://localhost:4200',
-      methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    }),
-  )
-  .use(bodyParser.json())
-  .use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-  request('http://api.icndb.com/jokes/random', (error, response, body) => {
-    if (!error) {
-      console.log(typeof body);
-      body = JSON.parse(body);
-
-      const joke = { id: body.value.id, joke: body.value.joke };
-      res.status(200).send(joke);
-    } else {
-      res
-        .status(403)
-        .send({ message: "chuck Norris didn't thousg that was funny" });
-    }
-  });
-});
+app.get('/', (req, res) => res.send('ayo e'));
 
 app.listen(port, () =>
   console.log(`chuck norris is listenning on port ${port}`),
